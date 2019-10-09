2e70147215c1d346b483f9ab3741c09b9e1fd5a2
tttg <thetruththeguilty@protonmail.com>
2019-10-09 11:34:42 +0700

    add more generic action

--- hanabiTypes.ts
+++ hanabiTypes.ts
@@ -9,7 +9,32 @@ const SUCCESS_SUFFIX = '_success';
 const ERROR_SUFFIX = '_error';
 const CLEAR_SUFFIX = '_clear'; // clear-able state
 const REFRESH_SUFFIX = '_refresh';
-const SAVE_SUFFIX = '_save';
+const SAVE_SUFFIX = '_save'; // get a dump
+const FINISH_SUFFIX = "_finish"; // this normally means the page reach the end
+
+export function createBasicTypes<T>(prefix: string, types: T): B<T> {
+  const res = {} as any;
+  Object.keys(types).forEach(property => {
+    res[property] = prefix + property;
+  })
+  return res
+}
+
+export function createFetchTypes<T>(prefix: string, types: T): F<T> {
+  const res = {} as any;
+  Object.keys(types).forEach(property => {
+    let result = {} as any;
+    result.loading = prefix + property + LOADING_SUFFIX;
+    result.success = prefix + property + SUCCESS_SUFFIX;
+    result.error = prefix + property + ERROR_SUFFIX;
+    result.clear = prefix + property + CLEAR_SUFFIX;
+    result.refresh = prefix + property + REFRESH_SUFFIX;
+    result.save = prefix + property + SAVE_SUFFIX;
+    result.finish = prefix + property + FINISH_SUFFIX;
+    res[property] = result;
+  })
+  return res
+}
 
 /**
  * create types
@@ -31,6 +56,7 @@ export function composeTypes<T1, T2>(config: { prefix: string; BasicTypes: T1; F
       result.clear = prefix + property + CLEAR_SUFFIX;
       result.refresh = prefix + property + REFRESH_SUFFIX;
       result.save = prefix + property + SAVE_SUFFIX;
+      result.finish = prefix + property + FINISH_SUFFIX;
 
       res[property] = result;
       return;
